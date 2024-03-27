6965619d96c78c6deb58cd7d2fbf3f9d38ca313b
Manel <quemonmanel@gmail.com>
2024-03-27 18:43:35 +0100

    chore:mods on connection

--- db/server.js
+++ db/server.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose"); 
 const server  =  async() => {
-    try{
-        await mongoose.connect("mongodb://apirestblog-production-3622.up.railway.app");
-        console.log("Welcome to the database, have fun!")
-    }catch(error) {
-        console.log(error);
-        throw new Error("Cant connect to the database"); 
-    }
+//     try{
+//         await mongoose.connect("mongodb://localhost:27017/my_blog");
+//         console.log("Welcome to the database, have fun!")
+//     }catch(error) {
+//         console.log(error);
+//         throw new Error("Cant connect to the database"); 
+//     }
+// } 
+
+try{
+    const { DB_USER, DB_PASS, DB_NAME } = process.env;
+    const connectionString = `mongodb://${DB_USER}:${DB_PASS}@apirestblog-production-3622.up.railway.app:27017/${DB_NAME}`;
+    await mongoose.set(connectionString,  'useNewUrlParser', true );
+    console.log("Welcome to the database, have fun!")
+}catch(error) {
+    console.log(error);
+    throw new Error("Cant connect to the database"); 
+}
 } 
 
 module.exports = {
