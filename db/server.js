22eb62a476648c29e74856f6010da0e17851d852
Manel <quemonmanel@gmail.com>
2024-04-02 19:25:22 +0200

    chore: why???

--- db/server.js
+++ db/server.js
@@ -1,29 +1,29 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const server = async () => {
-  //     try{
-  //         await mongoose.connect("mongodb://localhost:27017/my_blog");
-  //         console.log("Welcome to the database, have fun!")
-  //     }catch(error) {
-  //         console.log(error);
-  //         throw new Error("Cant connect to the database");
-  //     }
-  // }
+      try{
+          await mongoose.connect("mongodb://localhost:27017/my_blog");
+          console.log("Welcome to the database, have fun!")
+      }catch(error) {
+          console.log(error);
+          throw new Error("Cant connect to the database");
+      }
+  }
   // apirestblog-production-3622.up.railway.app:27017/
 
-  try {
-    dotenv.config();
-    const { DB_USER, DB_PASS, DB_NAME } = process.env;
-    const API_URL = `mongodb://${DB_USER}:${DB_PASS}mongodb://mongo:yXTFPqPUvuDBJRQrJSOtrnbMlCebenyy@roundhouse.proxy.rlwy.net:27861${DB_NAME}`;
+//   try {
+//     dotenv.config();
+//     const { DB_USER, DB_PASS, DB_NAME } = process.env;
+//     const API_URL = `mongodb://${DB_USER}:${DB_PASS}apirestblog-production-3622.up.railway.app:27017/${DB_NAME}`;
 
-    await mongoose.set(API_URL, "useNewUrlParser", true , "useFindAndModify", false);
-    console.log("Welcome to the database, have fun!");
+//     await mongoose.set(API_URL, "useNewUrlParser", true , "useFindAndModify", false);
+//     console.log("Welcome to the database, have fun!");
    
-  } catch (error) {
-    console.log(error);
-    throw new Error("Cant connect to the database");
-  }
-};
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error("Cant connect to the database");
+//   }
+// };
 
 module.exports = {
   server,
