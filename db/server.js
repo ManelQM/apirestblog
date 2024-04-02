0a3579ef580858c837d2cb7dc8dc5c48529481f7
Manel <quemonmanel@gmail.com>
2024-04-02 13:45:30 +0200

    chore:mods

--- db/server.js
+++ db/server.js
@@ -13,9 +13,9 @@ const server = async () => {
   try {
     dotenv.config();
     const { DB_USER, DB_PASS, DB_NAME } = process.env;
-    const connectionString = `mongodb://${DB_USER}:${DB_PASS}apirestblog-production-3622.up.railway.app:27017/${DB_NAME}`;
+    const API_URL = `mongodb://${DB_USER}:${DB_PASS}apirestblog-production-3622.up.railway.app:27017/${DB_NAME}`;
 
-    await mongoose.set(connectionString, "useNewUrlParser", true , "useFindAndModify", false);
+    await mongoose.set(API_URL, "useNewUrlParser", true , "useFindAndModify", false);
     console.log("Welcome to the database, have fun!");
    
   } catch (error) {
